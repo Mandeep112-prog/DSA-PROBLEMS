3792b7f464924b6938a04194d0a39f3dd03f8a9a
mandeep112-prog <mandeepkumarppt@gmail.com>
2025-09-09 19:56:51 +0530

    Reverse array with extra space

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ this folder is created for dsa-problem solving
 
 Problem 1: find greatest number in the array.
 Problem 2: Linear search
+problem 3: Reverse array with extra space
---
+++ problem3.cpp
@@ -0,0 +1,29 @@
+// Reverse an array with extra space 
+
+#include<iostream>
+using namespace std;
+
+void printArr(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<",";
+    }
+}
+
+int main(){
+    int arr[]={1,2,3,4,5,6};
+    int n = sizeof(arr)/sizeof(int);
+
+    int copyArr[n];
+
+    for(int i=0; i<n; i++){
+        int j = n-i-1;
+        copyArr[i]=arr[j];
+    }
+
+    for(int i=0; i<n; i++){
+        arr[i]=copyArr[i];
+    }
+
+    printArr(arr,n);
+    return 0;
+}

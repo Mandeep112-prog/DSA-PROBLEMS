1c74e28eed1cd34818f9d6a9f8f082ed769ecd9e
mandeep112-prog <mandeepkumarppt@gmail.com>
2026-04-14 01:53:07 +0530

    chore: clean repo and update gitignore

--- searching/stairCase_search.cpp
+++ searching/stairCase_search.cpp
@@ -1,27 +1,3 @@
-// //approach 1 : brute force approach time complexity O(n*m)
-// #include<iostream>
-// using namespace std;
-
-// int linearSearch(int mat[][4], int n, int key){
-//     for(int i=0; i<n; i++){
-//         for(int j=0; j<n; j++){
-//             if(mat[i][j] == key){
-//                 cout<<"Element found at index : "<<"row : "<<i<<" "<<"column : "<<j;
-//             }
-//         }
-//     }
-//     cout<<"Element not found";
-//     return -1;
-// }
-
-// int main(){
-//     int mat[4][4] = {{5,10,15,20},{7,14,21,28},{10,20,30,40},{15,30,45,60}};
-//     int key = 100;
-//     linearSearch(mat,4,key);
-//     return 0;
-// }
-
-
 
 //best approach : stairecase time complexity O(n+m)
 #include<iostream>

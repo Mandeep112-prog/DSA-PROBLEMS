b4e59d55436ee18d035f3d293eea0a012426dd96
mandeep112-prog <mandeepkumarppt@gmail.com>
2026-04-24 20:55:15 +0530

    construct using this pointer changes

--- basics/constructor.cpp
+++ basics/constructor.cpp
@@ -5,17 +5,23 @@ class Car{
     string name;
     string color;
     public:
-    Car(string name, string color){
-        this->name=name;//this pointer pointer point to the current object means c1
-        this->color=color;
+    Car(){
+        cout<<"without parameter"<<endl;
+    }
+    Car(string nameVal, string colorVal){
+        name=nameVal;
+        color=colorVal;
     }
     string getName(){
-        cout<<"Car name :"<<name;
+        cout<<"Car name :"<<name<<endl;
     }
 };
 
 int main(){
+    Car c0;
     Car c1("maruti 800","red");
+    Car c2("honda","white");
     c1.getName();
+    c2.getName();
     return 0;
 }

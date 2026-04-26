bc2bbfa589463d344f690ac0fe5feff2ff6e5055
mandeep112-prog <mandeepkumarppt@gmail.com>
2026-04-26 20:25:22 +0530

    copy constructor

---
+++ basics/copyConstructor.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class car {
+    public:
+    string name;
+    string color;
+
+    car(string name , string color){
+        this->name=name;
+        this->color=color;
+    }
+};
+
+int main(){
+    car c1("maruti800","white");
+    car c2(c1);//copy constructor
+    cout<<c2.name<<endl;
+    cout<<c2.color<<endl;
+    return 0;
+}

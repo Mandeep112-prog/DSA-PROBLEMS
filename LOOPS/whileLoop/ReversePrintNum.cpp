30f037b60feb4cbab967c6debe3e82c7cd1cd488
mandeep112-prog <mandeepkumarppt@gmail.com>
2026-05-10 01:29:41 +0530

    Printing number in reverse order

---
+++ LOOPS/whileLoop/ReversePrintNum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 10;
+    cout<<"Reverse numbers are from 10 to 1 are :";
+    while(i>=1){
+        cout<<i<<" ";
+        i--;
+    }
+    return 0;
+}

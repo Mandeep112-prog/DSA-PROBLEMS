14ff81915c835ff977850481665f1d780c7e4fbe
mandeep112-prog <mandeepkumarppt@gmail.com>
2026-05-12 20:35:02 +0530

    calculate sum of 1 to N odd number

---
+++ LOOPS/whileLoop/calSumOddNum.cpp
@@ -0,0 +1,22 @@
+//calculate sum of first 1 to N odd numbers
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter N odd number to calculate sum :";
+    cin>>num;
+    int sum=0;
+    int i=0;
+    cout<<"Sum of 1 to "<<num<< " odd numbers :";
+    while(num>=i){
+        if(i%2!=0){
+            sum+=i;
+        }
+        i++;
+    }
+    cout<<sum;
+
+    return 0;
+}

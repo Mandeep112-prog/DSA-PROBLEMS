32e8b69a278584901836af5c270c11267606b18c
mandeep112-prog <mandeepkumarppt@gmail.com>
2026-05-15 19:30:00 +0530

    Apply gitignore

---
+++ LOOPS/whileLoop/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+int num;
+    // cout<<"Enter N even number to calculate sum :";
+    // cin>>num;
+    // int sum=0;
---
+++ oops/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+if(mileage!=NULL){
+        //     delete mileage;
+        //     mileage = NULL;
+        // }

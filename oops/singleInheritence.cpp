068a8e8783501578d80051ece3a444539c8399ad
mandeep112-prog <mandeepkumarppt@gmail.com>
2026-05-16 20:30:46 +0530

    add inheritance program

---
+++ oops/singleInheritence.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Animal {
+    public:
+    string color;
+    void eat(){
+        cout<<"eating...";
+    }
+    void breadthing(){
+        cout<<"Breadthing...";
+    }
+};
+class Fish : public Animal {
+    public:
+    void swim(){
+        cout<<"swiming..."<<endl;
+        }
+};
+int main(){
+    Fish f1;
+    f1.swim();
+   f1.eat();
+    return 0;
+}

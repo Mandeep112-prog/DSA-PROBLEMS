a008d1f7584307dc0cc012294e5e99951ae3a4df
mandeep112-prog <mandeepkumarppt@gmail.com>
2026-05-16 21:02:48 +0530

    ultilevel Inheritance problem

---
+++ oops/multilevelInheritance.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class Animal {
+    public:
+    void eat(){
+        cout<<"Eats\n";
+    }
+    void breathe(){
+        cout<<"breaths\n";
+    }
+};
+class Mammal : public Animal{
+    public:
+    string bloodType;
+    Mammal(){
+        bloodType = "warm";
+    }
+};
+class Dog : public Mammal{
+    public:
+    void tailWag(){
+        cout<<"a dog wags its tail\n";
+    }
+};
+
+int main(){
+    Dog d1;
+    d1.eat();
+    d1.breathe();
+    d1.tailWag();
+    cout<<d1.bloodType <<endl;
+    return 0;
+}

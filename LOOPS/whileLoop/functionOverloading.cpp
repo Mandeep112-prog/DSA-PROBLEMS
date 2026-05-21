617f378f34116789f29ced0eb8b6879e441bb676
mandeep112-prog <mandeepkumarppt@gmail.com>
2026-05-21 18:55:16 +0530

    Function overloading problem

---
+++ LOOPS/whileLoop/functionOverloading.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Print{
+    public:
+    int x;
+    string str;
+    void show(int x){
+        cout<<"int x :"<<x<<endl;
+    }
+
+    void show(string str){
+        cout<<"String :"<<str<<endl;
+    }
+};
+
+int main(){
+    Print p1;
+    p1.show(10);
+    p1.show("Mandeep");
+    return 0;
+}

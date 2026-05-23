813ffd1bbf53aaa313dc45c7949c59660be5ab63
mandeep112-prog <mandeepkumarppt@gmail.com>
2026-05-23 16:49:00 +0530

    Count and print total number of given number

---
+++ practice/countPrintTotalNum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter a number :";
+    cin>>num;
+    int count = 0;
+    cout<<"All digits are : ";
+    while(num>0){
+        int lastDigit = num%10;
+        cout<<lastDigit<<" ";
+        num = num/10;
+        count++;
+    }
+    cout<<endl;
+    cout<<"No of digit :"<<count;
+    return 0;
+}

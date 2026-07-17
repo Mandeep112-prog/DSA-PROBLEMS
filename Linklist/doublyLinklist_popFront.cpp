985ef841af45b5a7236e09bd2d0ff7918684ec8c
mandeep112-prog <mandeepkumarppt@gmail.com>
2026-07-17 11:36:07 +0530

    doubly linklist delete node from beginning

---
+++ Linklist/doublyLinklist_popFront.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node*next;
+    Node*prev;
+
+    Node(int val){
+        data = val;
+        next = prev = NULL;
+    }
+};
+class DoubleList{
+    public:
+    Node*head;
+    Node*tail;
+
+    DoubleList(){
+        head = tail = NULL;
+
+    }
+//add node from the beginning
+    void push_front(int val){
+        Node *newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+        }
+        else{
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+//this function is used for delete node from the beginning
+    void pop_front(){
+        Node*temp =head;
+        head = head->next;
+        if(head!=NULL){
+            head->prev = NULL;
+        }
+        temp->next = NULL;
+        delete temp;
+    }
+//print list
+    void print(){
+        Node*temp = head;
+        while(temp!=NULL){
+            cout<<temp->data<<"<=>";
+            temp = temp->next;
+        }
+        cout<<"NULL\n";
+    }
+};
+
+int main(){
+    DoubleList dbll;
+    dbll.push_front(5);
+    dbll.push_front(4);
+    dbll.push_front(3);
+    dbll.push_front(2);
+    dbll.push_front(1);
+    dbll.print();
+    dbll.pop_front();
+    dbll.print();
+    return 0;
+
+}
